598c0eeaf1138e5616a99139af445b2d15e14fcd
Naoya Nakazawa <naoya.n@gmail.com>
2015-11-16 15:40:03 +0900

    Add yubico

---
+++ modules/projects/manifests/yubico.pp
@@ -0,0 +1,25 @@
+class projects::yubico {
+  notify { 'class project::yubico declared': }
+
+  package {
+    [
+      'yubico-pam',
+    ]
+  }
+
+  package {
+    [
+      'yubico-authenticator',
+      'yubikey-neo-manager',
+     ]:
+    provider => 'brewcask',
+    require  => Homebrew::Tap['caskroom/cask'],
+  }
+
+  file { "/Users/#{::boxen_user}/.yubico":
+      ensure => directory,
+      mode   => 0700,
+  }
+
+  notify { "Please copy to #{::homebrew_root}/Cellar/pam_yubico/[version]/lib/security/pam_yubico.so /usr/lib/pam/pam_yubico.so": }
+}
