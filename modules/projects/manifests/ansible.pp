3c0f978ff1f7e9471ada2a2dba63e49da59d92f0
Naoya Nakazawa <naoya.n@gmail.com>
2015-12-18 10:21:14 +0900

    Ansible add human_log.py callback plugin

--- modules/projects/manifests/ansible.pp
+++ modules/projects/manifests/ansible.pp
@@ -6,4 +6,29 @@ class projects::ansible {
   include projects::python
 
   python::install_package { 'ansible-lint': }
+
+  file { "/Users/${::boxen_user}/.ansible":
+      ensure  => directory,
+  }
+
+  file { "/Users/${::boxen_user}/.ansible/plugins/callback_plugins":
+      ensure  => directory,
+      recurse => true,
+      require => File["/Users/${::boxen_user}/.ansible"],
+  }
+
+  file { "/Users/${::boxen_user}/.ansible/ansible.cfg":
+      content => "[defaults]
+callback_plugins= ~/.ansible/plugins/callback_plugins/
+retry_files_enabled = False
+",
+      require => File["/Users/${::boxen_user}/.ansible"],
+  }
+
+  require wget
+
+  exec { 'get human_log.py':
+    command => "wget -q https://raw.githubusercontent.com/redhat-openstack/khaleesi/master/plugins/callbacks/human_log.py -O /Users/${::boxen_user}/.ansible/plugins/callback_plugins",
+    creates => "/Users/${::boxen_user}/.ansible/plugins/callback_plugins/human_log.py",
+}
 }
