8d66fe9fad0a6e30a52380e45d8b64b248c9fbbb
Naoya Nakazawa <naoya.n@gmail.com>
2015-12-18 10:18:33 +0900

    Fix typo

--- modules/projects/manifests/yubico.pp
+++ modules/projects/manifests/yubico.pp
@@ -16,10 +16,10 @@ class projects::yubico {
     require  => Homebrew::Tap['caskroom/cask'],
   }
 
-  file { "/Users/#{::boxen_user}/.yubico":
+  file { "/Users/${::boxen_user}/.yubico":
       ensure => directory,
       mode   => 0700,
   }
 
-  notify { "Please copy to #{::homebrew_root}/Cellar/pam_yubico/[version]/lib/security/pam_yubico.so /usr/lib/pam/pam_yubico.so": }
+  notify { "Please copy to ${::homebrew_root}/Cellar/pam_yubico/[version]/lib/security/pam_yubico.so /usr/lib/pam/pam_yubico.so": }
 }
