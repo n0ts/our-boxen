0a7fdded9af5b4ea0c2c0f028b6310ae39b0acbc
n0ts <me@n0ts.org>
2016-02-06 23:35:56 +0900

    Fix missing src/boxen directory

--- modules/people/manifests/n0ts/base.pp
+++ modules/people/manifests/n0ts/base.pp
@@ -67,6 +67,10 @@ for c in \"${brewcask::config::cask_room}/\"*; do vl=(`ls -t \$c`) && for v in \
     require => File['/Users/Shared/prj'],
   }
 
+  file { "/Users/${::boxen_user}/src":
+    ensure  => directory,
+  }
+
   file { "/Users/${::boxen_user}/src/boxen":
     ensure  => link,
     target  => "/Users/Shared/prj/github",
