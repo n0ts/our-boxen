f8c76840167c257d842dc8b7b99925eeb0c8a66f
n0ts <me@n0ts.org>
2016-02-06 22:51:10 +0900

    Refactoring ruby module

--- modules/projects/manifests/ruby.pp
+++ modules/projects/manifests/ruby.pp
@@ -1,22 +1,30 @@
 class projects::ruby {
   notify { 'class project::ruby declared': }
 
-  # brew install ruby-build --force --HEAD
+  $version_latest = '2.3.0'
+  $version_boxen = '2.0.0-p647'
 
-  define latest_install_package($ruby_version = '2.3.0') {
+  file { "${boxen::config::home}/bin/brew-ruby-update.sh":
+    content => "#!/bin/bash
+brew install ruby-build --force --HEAD
+",
+    mode    => 0755,
+  }
+
+  define latest_install_package($ruby_version = $projects::ruby::version_latest) {
     ruby_gem { "${name} for ${ruby_version}":
       gem          => $name,
       ruby_version => $ruby_version,
     }
   }
 
-  # plugins
+  # rbenv plugins
   file { "${::boxen_home}/rbenv/plugins":
     ensure  => directory,
     require => $require
   }
   ruby::rbenv::plugin { 'rbenv-vars':
-    ensure => 'v1.2.0',
+    ensure  => 'v1.2.0',
     source  => 'sstephenson/rbenv-vars',
   }
   ruby::rbenv::plugin { 'rbenv-gem-rehash':
@@ -30,7 +38,7 @@ class projects::ruby {
 
   # latest stable
   class { 'ruby::global':
-     version => '2.3.0',
+     version => $version_latest,
   }
 
   latest_install_package {
@@ -41,10 +49,10 @@ class projects::ruby {
   }
 
   # required for Boxen module development
-  ruby::version { '2.0.0-p647': }
+  ruby::version { $version_boxen: }
 
-  ruby_gem { "bundler for 2.0.0-p647":
+  ruby_gem { "bundler for ${version_boxen}":
     gem          => 'bundler',
-    ruby_version => '2.0.0-p647',
+    ruby_version => $version_boxen,
   }
 }
