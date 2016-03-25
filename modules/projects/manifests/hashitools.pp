0da357ab4c2072c190db32634ce224e5d458e184
n0ts <me@n0ts.org>
2016-03-25 12:10:47 +0900

    Add installing vagrant plugins

--- modules/projects/manifests/hashitools.pp
+++ modules/projects/manifests/hashitools.pp
@@ -1,6 +1,23 @@
 class projects::hashitools {
   notify { 'class project::hashitools declared': }
 
+
+  define install_vagrant_plugin() {
+    $vagrant_root = "/opt/vagrant"
+    $vagrant_bin = "${vagrant_root}/bin/vagrant"
+    $unless = "${vagrant_bin} plugin list | grep ${name}"
+    exec { "install-vagrant-pluguin-${name}":
+      command     => "${vagrant_bin} plugin install ${name}",
+      environment =>
+        [
+         "VAGRANT_HOME=/Users/${::boxen_user}/.vagrant.d",
+         "VAGRANT_NO_COLOR=1",
+         ],
+      unless      => $unless,
+      require     => [ Package['vagrant'], File["/Users/${::boxen_user}/.vagrant.d"] ],
+    }
+  }
+
   include brewcask
   package {
     [
@@ -16,4 +33,24 @@ class projects::hashitools {
     provider => 'brewcask',
     require  => Homebrew::Tap['caskroom/cask'],
   }
+
+  file { "/Users/${::boxen_user}/.vagrant.d":
+    ensure  => directory,
+  }
+
+  install_vagrant_plugin {
+    [
+     "sahara",
+     "vagrant-aws",
+     "vagrant-azure",
+     "vagrant-digitalocean",
+     "vagrant-google",
+     "vagrant-managed-servers",
+     "vagrant-rackspace",
+     "vagrant-softlayer",
+     "vagrant-vmware-fusion",
+     ]: ;
+  }
+
+  notify { "vagrant plugin license vagrant-vmware-fusion ~/license.lic": }
 }
