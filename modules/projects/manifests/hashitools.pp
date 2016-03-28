90a1d95418b4775cd124bb672e45123e6a8ca57b
n0ts <me@n0ts.org>
2016-03-28 12:37:59 +0900

    Add installing vagrant plugin aws dummy box

--- modules/projects/manifests/hashitools.pp
+++ modules/projects/manifests/hashitools.pp
@@ -16,7 +16,24 @@ class projects::hashitools {
       unless      => $unless,
       require     => [ Package['vagrant'], File["/Users/${::boxen_user}/.vagrant.d"] ],
     }
+
+    # dummy AWS box
+    # https://github.com/mitchellh/vagrant-aws
+    if $name == "vagrant-aws" {
+      $unless_box = "${vagrant_bin} box list | grep dummy-aws"
+      exec { "install-vagrant-pluguin-aws-dummy-box":
+        command     => "${vagrant_bin} box add dummy-aws https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box",
+        environment =>
+          [
+           "VAGRANT_HOME=/Users/${::boxen_user}/.vagrant.d",
+           "VAGRANT_NO_COLOR=1",
+           ],
+        unless      => $unless_box,
+        require     => [ Package['vagrant'], File["/Users/${::boxen_user}/.vagrant.d"] ],
+      }
+    }
   }
+
 
   include brewcask
   package {
