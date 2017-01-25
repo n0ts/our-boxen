b668dd9b8b4d64541ccc887c7a032fb33c904a8e
Naoya Nakazawa <naoya.n@gmail.com>
2017-01-25 16:54:45 +0900

    Add python 3.6.0

--- modules/projects/manifests/python.pp
+++ modules/projects/manifests/python.pp
@@ -1,6 +1,8 @@
 class projects::python {
   notify { 'class project::python declared': }
 
+  $version_latest = '3.6.0'
+
   define install_package($url = undef) {
     python::package { "${name} for 2.7.10":
       package => $name,
@@ -9,15 +11,17 @@ class projects::python {
     }
   }
 
-  # Lambda use python 2.7.10
+  # lambda use python 2.7.10
   class { 'python::global':
     version => '2.7.10',
   }
 
+  # latest stable
+  python::version { $version_latest: }
+
   install_package {
     [
       'supervisor',
-      'virtualenv',
     ]: ;
   }
 }
