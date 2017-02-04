520592feda320051cffcb7e63512b80679c3f1d6
Naoya Nakazawa <naoya.n@gmail.com>
2017-02-04 09:31:49 +0900

    Install all gcp components

--- modules/projects/manifests/google_cloud.pp
+++ modules/projects/manifests/google_cloud.pp
@@ -5,9 +5,10 @@ class projects::google_cloud {
     command => join([
                      'curl -sL https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz > /tmp/google-cloud-sdk.tar.gz',
                      'tar zxf google-cloud-sdk.tar.gz',
-                     './google-cloud-sdk/install.sh --usage-reporting false --command-completion false --path-update false --additional-components app',
+                     './google-cloud-sdk/install.sh --usage-reporting false --command-completion false --path-update false --additional-components app-engine-java app-engine-php-darwin app-engine-python app kubectl alpha beta gcd-emulator pubsub-emulator cloud-datastore-emulator app-engine-go cbt datalab docker-credential-gcr',
                      'mv /tmp/google-cloud-sdk /opt/',
                      'rm -f /tmp/google-cloud-sdk.tar.gz',
+                     "chown -R ${::boxen_user} /Users/${::boxen_user}/.config/gcloud",
                      ], "\n"),
     cwd     => '/tmp',
     user    => root,
