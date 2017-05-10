36dab5f7d6224333e675c937f99c62e1df16d3d9
n <me@n0ts.org>
2017-05-10 10:19:53 +0900

    Add circleci manifest

---
+++ modules/projects/manifests/circleci.pp
@@ -0,0 +1,12 @@
+class projects::circleci {
+  notify { 'class project::circleci declared': }
+
+  $download_url = uriescape("https://circle-downloads.s3.amazonaws.com/releases/build_agent_wrapper/circleci")
+  exec { "install-circleci":
+    command => join([
+                     "curl -L \"${download_url}\" > \"${::boxen_home}/bin/circleci\"",
+                     "chmod +x \"${::boxen_home}/bin/circleci\"",
+                     ], "\n"),
+    unless  => "test -f \"${::boxen_home}/bin/circleci\"",
+  }
+}
